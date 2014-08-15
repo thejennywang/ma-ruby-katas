7de05b23d40afca5fb77667f784ce68e5672d747
thejennywang <thejennywang@gmail.com>
2014-08-15 11:49:35 +0100

    23 tests to go!

--- questions.rb
+++ questions.rb
@@ -68,6 +68,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	[array.select { |n| n.even?}] << array.select {|n| n.odd? }
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -75,19 +76,29 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+	array.select { |word| word == word.reverse }.count
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+	array.inject do |memo, word|
+		memo.length < word.length ? memo : word
+	end
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
+	array.inject do |memo, word|
+		memo.length > word.length ? memo : word
+	end
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+	array.inject do |sum, number|
+		sum + number
+	end
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
