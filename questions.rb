e0089a95e0ab7bc4d4e8443f16f41cadf917fe6a
Jenny Wang <thejennywang@gmail.com>
2014-08-18 18:36:19 +0100

    Updated check_a_string_for_special_characters
    Change in syntax made old answer fail test

--- questions.rb
+++ questions.rb
@@ -194,7 +194,7 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
-  string =~ /\W/
+  /\W/ === string
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
